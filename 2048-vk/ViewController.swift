9471f775afae82872a5e38d0da2ee4777dff193c
Usov Nikita <usov107@gmail.com>
2023-05-06 14:31:02 +0700

    Initial Commit

---
+++ 2048-vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2048-vk
+//
+//  Created by admin on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
